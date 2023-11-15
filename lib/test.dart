9191e0965d3f6dfe5b2b0e8a88bbec9ddb9e7e74
535220166 <149558071+ChristabellaJC@users.noreply.github.com>
2023-11-15 13:33:31 +0700

    test.dart

--- lib/test.dart
+++ lib/test.dart
@@ -47,17 +47,32 @@ class ProductListScreen extends StatelessWidget {
       description: 'Coca Cola',
       price: 12.5,
       imageUrl:
-          'https://images.tokopedia.net/img/cache/700/hDjmkQ/2022/12/6/d3b1c185-d9c0-4607-b18c-f7a8062ac641.jpg',
+          'https://images.tokopedia.net/img/cache/700/VqbcmM/2022/2/26/c56bd5f1-c57e-4a90-b8b3-62c5894a76e7.jpg',
     ),
     Product(
       id: '3',
       name: 'Sprite',
-      description: 'Description for Product 3',
+      description: 'Sprite',
       price: 11.8,
       imageUrl:
-          'https://images.tokopedia.net/img/cache/700/hDjmkQ/2022/12/6/d3b1c185-d9c0-4607-b18c-f7a8062ac641.jpg',
+          'https://images.tokopedia.net/img/cache/700/VqbcmM/2023/5/10/9eb5bbfe-f5cf-4612-a8de-646f06e88c07.jpg',
     ),
-
+    Product(
+      id: '4',
+      name: 'RootBeer',
+      description: 'RB',
+      price: 17.9,
+      imageUrl:
+          'https://images.tokopedia.net/img/cache/700/VqbcmM/2023/8/19/a6b35b6e-bf83-4756-8270-65f95a60d635.jpg',
+    ),
+    Product(
+      id: '5',
+      name: 'Milkis',
+      description: 'Milkis',
+      price: 6.9,
+      imageUrl:
+          'https://images.tokopedia.net/img/cache/700/product-1/2021/3/30/11023039/11023039_e5124815-68a3-4f1d-9f0b-43aeecb9b613.jpeg',
+    ),
   ];
 
   @override
@@ -65,25 +80,19 @@ class ProductListScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Godrej'),
-        backgroundColor: Colors.red, 
+        backgroundColor: Colors.red,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          onPressed: () {
-
-          },
+          onPressed: () {},
         ),
         actions: [
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () {
-
-            },
+            onPressed: () {},
           ),
           IconButton(
             icon: Icon(Icons.mail),
-            onPressed: () {
-
-            },
+            onPressed: () {},
           ),
         ],
       ),
@@ -94,9 +103,8 @@ class ProductListScreen extends StatelessWidget {
               padding: EdgeInsets.all(16.0),
               child: CircleAvatar(
                 radius: 50,
-                backgroundColor: Colors.red, 
                 backgroundImage: AssetImage(
-                    'assets/store_image.jpg'), 
+                    'https://www.its.ac.id/it/wp-content/uploads/sites/46/2021/06/blank-profile-picture-973460_1280.png'),
               ),
             ),
             const Text(
@@ -107,15 +115,13 @@ class ProductListScreen extends StatelessWidget {
               ),
             ),
             const Text(
-              'Jumlah Pengikut: 1000', 
+              'Jumlah Pengikut: 1000',
             ),
             ElevatedButton(
-              onPressed: () {
-              },
+              onPressed: () {},
               child: Text('Mengikuti'),
             ),
-            const SizedBox(
-                height: 16.0),
+            const SizedBox(height: 16.0),
             CarouselSlider(
               items: [
                 Image.network(
@@ -132,10 +138,7 @@ class ProductListScreen extends StatelessWidget {
                 viewportFraction: 0.8,
               ),
             ),
-
-            const SizedBox(
-                height:
-                    16.0), 
+            const SizedBox(height: 16.0),
             CarouselSlider(
               items: [
                 Image.network(
@@ -167,12 +170,10 @@ class ProductListScreen extends StatelessWidget {
                         const Icon(Icons.error),
                   ),
                   trailing: ElevatedButton(
-                    onPressed: () {
-                    },
+                    onPressed: () {},
                     child: Text('Masukkan dalam keranjang'),
                   ),
-                  onTap: () {
-                  },
+                  onTap: () {},
                 );
               },
             ),
